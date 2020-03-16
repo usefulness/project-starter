208aff23060e7ec6235f3b83475d16bd1e843a86
Mateusz Kwieciński <36954793+mateuszkwiecinski@users.noreply.github.com>
2020-03-17 00:23:16 +0100

    fix: Fix detekt task failure when calling `./gradlew clean build` by providing Detekt config lazily (#34)

--- plugins/src/main/kotlin/com/project/starter/quality/internal/ResourceLoader.kt
+++ plugins/src/main/kotlin/com/project/starter/quality/internal/ResourceLoader.kt
@@ -3,15 +3,20 @@ package com.project.starter.quality.internal
 import java.io.File
 import java.net.JarURLConnection
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 
 private object ResourceLoader
 
-internal fun Project.loadFromResources(path: String): File? {
+internal fun Project.loadFromResources(path: String): Provider<File?> {
     val configFile = ResourceLoader::class.java.classLoader.getResource(path)
 
-    @Suppress("UseIfInsteadOfWhen")
-    return when (val jar = configFile?.openConnection()) {
-        is JarURLConnection -> resources.text.fromArchiveEntry(jar.jarFileURL, jar.entryName).asFile()
-        else -> configFile?.let { File(it.file) }
+    return provider {
+        @Suppress("UseIfInsteadOfWhen")
+        when (val jar = configFile?.openConnection()) {
+            is JarURLConnection -> resources.text.fromArchiveEntry(jar.jarFileURL, jar.entryName).asFile()
+            else -> configFile?.let { File(it.file) }
+        }?.also {
+            logger.info("Loaded config: $it")
+        }
     }
 }
