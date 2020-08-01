fb21fb3c62bcc08a3974b7f3b0ef7c7994b9d263
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-01 11:09:51 +0200

    chore: bump gradle version to 6.5.1

--- issuechecker/src/test/kotlin/com/starter/issuechecker/FunctionalTest.kt
+++ issuechecker/src/test/kotlin/com/starter/issuechecker/FunctionalTest.kt
@@ -23,4 +23,18 @@ internal class FunctionalTest {
             )
         )
     }
+
+    @Test
+    internal fun `finds all issue links`() {
+        val text =
+            """
+            Some text with 
+            comment /* in https://youtrack.jetbrains.com/issue/KT-36808/ the middle 
+            lol
+            """.trimIndent()
+
+        val result = IssueChecker(IssueChecker.Config()).findAllLinksBlocking(text).single().map { it.toString() }
+
+        assertThat(result).containsExactly("https://youtrack.jetbrains.com/issue/KT-36808/")
+    }
 }
