80358fd11304fc60dbd9e26adff0e9739b6e964a
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-07-28 22:29:21 +0200

    chore: Cleanup unused comment

--- jvm/src/main/kotlin/com/project/starter/modules/plugins/KotlinLibraryPlugin.kt
+++ jvm/src/main/kotlin/com/project/starter/modules/plugins/KotlinLibraryPlugin.kt
@@ -1,6 +1,5 @@
 package com.project.starter.modules.plugins
 
-import com.project.starter.config.getByType
 import com.project.starter.config.plugins.rootConfig
 import com.project.starter.config.withExtension
 import com.project.starter.modules.extensions.KotlinLibraryConfigExtension
@@ -10,7 +9,7 @@ import com.project.starter.modules.tasks.ProjectCoverageTask.Companion.registerP
 import com.project.starter.modules.tasks.ProjectTestTask.Companion.registerProjectTestTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.JavaPluginConvention
 
 class KotlinLibraryPlugin : Plugin<Project> {
 
@@ -33,7 +32,7 @@ class KotlinLibraryPlugin : Plugin<Project> {
             val javaFilesAllowed = config.javaFilesAllowed ?: rootConfig.javaFilesAllowed
             if (!javaFilesAllowed) {
                 val forbidJavaFiles = registerForbidJavaFilesTask { task ->
-                    val plugin = project.extensions.getByType<JavaPluginExtension>()
+                    val plugin = project.convention.getPlugin(JavaPluginConvention::class.java)
                     plugin.sourceSets.configureEach { sourceSet ->
                         if (sourceSet.name == "main" || sourceSet.name == "test") {
                             task.source += sourceSet.java
