fa9415568750d447f90bad96cd933bfe08f81523
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-08-19 00:08:15 +0300

    chore: Clean up depreceted properties

--- config/src/main/kotlin/com/project/starter/config/PluginUtils.kt
+++ config/src/main/kotlin/com/project/starter/config/PluginUtils.kt
@@ -7,6 +7,9 @@ import org.gradle.api.plugins.ExtensionContainer
 inline fun <reified T> ExtensionContainer.getByType() =
     getByType(T::class.java)
 
+inline fun <reified T> ExtensionContainer.findByType() =
+    findByType(T::class.java)
+
 inline fun <reified T> Project.withExtension(crossinline action: Project.(T) -> Unit) =
     afterEvaluate {
         it.action(it.extensions.getByType())
