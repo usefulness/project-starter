5288f34ccdb5d5947f97829e89e98e6cb609772e
Mateusz Kwieciński <36954793+mateuszkwiecinski@users.noreply.github.com>
2022-08-05 12:04:35 +0200

    Toggle ktlint rules (#401)

--- quality/src/main/kotlin/com/project/starter/quality/internal/Ktlint.kt
+++ quality/src/main/kotlin/com/project/starter/quality/internal/Ktlint.kt
@@ -11,7 +11,7 @@ internal fun Project.configureKtlint() {
     extensions.configure(KotlinterExtension::class.java) {
         it.experimentalRules = true
         it.reporters = emptyArray()
-        it.disabledRules = arrayOf("import-ordering", "experimental:argument-list-wrapping", "experimental:trailing-comma")
+        it.disabledRules += arrayOf("import-ordering", "filename", "experimental:function-signature")
     }
 
     tasks.named(ProjectCodeStyleTask.TASK_NAME) {
